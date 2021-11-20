0e896fbd4763ca58d628615dc47250c5fd376f00
evalestari17 <evalestari2375@gmail.com>
2021-11-20 18:04:05 +0700

    sliddder
    slidder

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -3,11 +3,84 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ProdukModel;
+use App\Models\ModelSimulasiKredit;
+use App\Models\ModelSlider;
 
 class DashboardController extends BaseController
 {
+	protected $ProdukModel;
+	protected $ModelSimulasiKredit;
+
+    public function __construct() {
+ 
+        // Memanggil form helper
+        helper('form');
+		$this->form_validation = \Config\Services::validation();
+        // Menyiapkan variabel untuk menampung upload model
+        $this->ProdukModel = new ProdukModel();
+        $this->ModelSimulasiKredit = new ModelSimulasiKredit();
+    }
+
 	public function index()
 	{
-		echo view('frontend/dealerindex');
+		$slider = new ModelSlider();
+		$data = [
+			'slider' => $slider->findAll(),
+			'title' => 'Home'
+		];
+		echo view('template/frontendTemplate/header',$data);
+		echo view('frontend/index');
+		echo view('template/frontendTemplate/footer');
 	}
+
+	public function vehicles()
+	{
+		$produk = new ProdukModel();
+		$data = [
+			'produk' => $produk->findAll(),
+			'title' => 'Our Vehicles',
+			'judul'	=> 'Our Vehicles'
+		];
+		// dd($data);
+		echo view('template/frontendTemplate/headerdetail',$data);
+		echo view('frontend/OurVehicles', $data);
+		echo view('template/frontendTemplate/footer');
+	}
+
+	public function about()
+	{
+		$data = [
+			'title' => 'About',
+			'judul'	=> 'About'
+		];
+		echo view('template/frontendTemplate/headerdetail',$data);
+		echo view('frontend/about', $data);
+		echo view('template/frontendTemplate/footer');
+	}
+
+	public function contact()
+	{
+		$data = [
+			'title' => 'Contact',
+			'judul'	=> 'Contact'
+		];
+		echo view('template/frontendTemplate/headerdetail',$data);
+		echo view('frontend/contact', $data);
+		echo view('template/frontendTemplate/footer');
+	}
+
+	public function simulasi()
+	{
+		$simulasi = new ModelSimulasiKredit();
+		$data = [
+			'skredit' => $simulasi->findAll(),
+			'title' => 'Simulasi Kredit',
+			'judul'	=> 'Simulasi Kredit'
+		];
+		echo view('template/frontendTemplate/headerdetail',$data);
+		echo view('frontend/simulasi', $data);
+		echo view('template/frontendTemplate/footer');
+	}
+
 }
